fd8858b797381c89aff892b003ba53940d2e0e90
2or3 <t.nagashima.cog@gmail.com>
2013-10-10 15:41:48 +0900

    import fuel php

---
+++ fuel/app/logs/2013/10/10.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2013-10-10 15:39:11 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
